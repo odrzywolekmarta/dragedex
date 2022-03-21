b3556f22324d397c18f73722dc281b8847b6ab49
Bartosz Odrzywolek <bartosz.odrzywolek@gmail.com>
2022-03-21 20:48:35 +0100

    Initial Commit

---
+++ DragedexTests/DragedexTests.swift
@@ -0,0 +1,33 @@
+//
+//  DragedexTests.swift
+//  DragedexTests
+//
+//  Created by Majka on 21/03/2022.
+//
+
+import XCTest
+@testable import Dragedex
+
+class DragedexTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
