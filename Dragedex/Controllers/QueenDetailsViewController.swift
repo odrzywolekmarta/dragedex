8ceb849771aed2867b6442333e6b5f5445582df7
Marta Odrzywolek <odrzywolek.m@gmail.com>
2022-03-22 18:57:50 +0100

    basic UI for queens view

--- Dragedex/Controllers/QueenDetailsViewController.swift
+++ Dragedex/Controllers/QueenDetailsViewController.swift
@@ -8,14 +8,19 @@
 import UIKit
 
 class QueenDetailsViewController: UIViewController {
-
+    @IBOutlet var queensImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var quoteLabel: UILabel!
+    @IBOutlet var seasonsLabel: UILabel!
+    @IBOutlet var winnerLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         doConfiguration()
-        // Do any additional setup after loading the view.
     }
     
     func doConfiguration() {
-        view.backgroundColor = .cyan
     }
 }
